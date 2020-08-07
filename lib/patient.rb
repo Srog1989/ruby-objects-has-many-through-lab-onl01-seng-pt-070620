f55a86f4227d929d2606ba86fd051fe9732dd5ad
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-07 18:53:12 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,22 @@
+class Patient
+ @@all = [] 
+ attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+     @@all << self 
+  end
+
+  def self.all
+    @@all
+  end 
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
+ 
+
+
+
+end
