4183e35bb9125fe778ee4167f438d76bbedc8565
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-07 19:01:10 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,6 +15,12 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
+    def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+  def doctors
+    Appointment.all.map{|appointment| appointment.doctor}
+  end
  
 
 
